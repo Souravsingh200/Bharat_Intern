65139a0dd6e8b423eeb567071d86296084fc5b57
Souravsingh200 <143682953+Souravsingh200@users.noreply.github.com>
2023-08-31 10:48:59 +0530

    Add files via upload

---
+++ Bharat-Intern---ML-master/Task-2 - Wine Quality Prediction/Wine Quality Prediction/app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,request,jsonify
+import pickle
+import numpy as np
+import json
+import pandas as pd
+
+regmodel = pickle.load(open('model.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+  
+
+@app.route('/predict' , methods=['POST'])
+def predict():  
+  float_features = [float(x) for x in request.form.values()]
+  features = [np.array(float_features)]
+  prediction = regmodel.predict(features)
+  return render_template("home.html",prediction_text = "wine quality is {}".format(prediction))
+
+if __name__ == " __main__":
+  app.run(debug=True)  
